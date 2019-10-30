fd35c609d85d98775787ad45d132ba9b85b0046d
Raheel Sayeed <raheelsayeed@osxlap04366.tch.harvard.edu>
2019-10-30 14:50:09 -0400

    examples

---
+++ SMARTMarkers/AdaptiveQuestionnaire/AdaptiveQuestionnaireTask.swift
@@ -0,0 +1,9 @@
+//
+//  AdaptiveOperation.swift
+//  SMARTMarkers
+//
+//  Created by Raheel Sayeed on 10/15/19.
+//  Copyright © 2019 Boston Children's Hospital. All rights reserved.
+//
+
+import Foundation
